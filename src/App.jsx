3845d4788639950e5be3eec295ddbfa25e95c11b
Srinivasan <jpsrinivasan38@gmail.com>
2023-08-22 17:11:24 +0530

    Basic Navigation bar is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
+import Navigation from './components/Navigation'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <div>
+      <Navigation />
+    </div>
   )
 }
 
