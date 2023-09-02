08cb68b4fdf64b73b09920a7cc435221881da7ff
Srinivasan <jpsrinivasan38@gmail.com>
2023-09-02 18:10:40 +0530

    UI sm bug fix

--- src/components/fragments/CourseViewPage.jsx
+++ src/components/fragments/CourseViewPage.jsx
@@ -67,7 +67,7 @@ function CourseViewPage({wideBar, setFragement, id}) {
 
     <div className={wideBar?darkColors["mainD"]+" md:h-full blur-sm md:filter-none md:ml-[12em]":darkColors["mainD"]+" md:h-full"}>
       <TopNav fragmentName={"ROXs Academy"}/>
-      <div className=' flex flex-col justify-center items-center mt-20 w-[70%] p-5'>
+      <div className=' flex flex-col justify-center items-center mt-20 md:w-[70%]  p-5'>
         <img src={courseDetails.thumbnail} alt="thumbnail for course" className=' w-[35em] my-5 rounded-[1em] shadow-2xl shadow-green-500'/>
         <h1 className=' font-extrabold text-2xl'>{courseDetails.Title}</h1>
         <p className=' my-5 text-lg text-justify'>{courseDetails.sDescription}</p>
@@ -76,14 +76,14 @@ function CourseViewPage({wideBar, setFragement, id}) {
         <p className='mr-auto'>by {courseDetails.author}</p>
         
       </div>
-      <div className=' bg-gradient-to-r w-[60%] rounded-[1em] p-5 shadow-2xl shadow-green-500'>
+      <div className=' bg-gradient-to-r md:w-[60%] rounded-[1em] p-5 shadow-2xl shadow-green-500'>
         <h2 className=' text-center text-2xl font-bold m-3'>Contents</h2>
         <ol >
           {(contentDetails.contents||[]).map((moduleName, index)=>(
 
               <li className=' odd:bg-gradient-to-r from-gray-800 rounded-xl even:bg-slate-900 shadow-md hover:scale-105'>
-                <div className='flex justify-between p-3 m-5 cursor-pointer' onClick={()=> toggleModuleVisibility(index)}>
-                <h1 className='inline'>{Object.keys(moduleName)}</h1>
+                <div className='flex justify-between p-3 m-5 cursor-pointer flex-wrap' onClick={()=> toggleModuleVisibility(index)}>
+                <h1 className='inline-block w-[70%]'>{Object.keys(moduleName)}</h1>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 inline-block">
   <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
 </svg>
@@ -91,7 +91,7 @@ function CourseViewPage({wideBar, setFragement, id}) {
 
             
             <ul className={`${
-          visibleModules[index] ? 'opacity-100 max-h-[1000px] transition-all duration-300 ease-in-out' : 'opacity-0 max-h-0 hidden overflow-hidden transition-all duration-300 ease-in-out'
+          visibleModules[index] ? 'opacity-100 max-h-[1000px] transition-all duration-300 ease-in-out' : 'opacity-0 max-h-0 min-h-0 scale-0 overflow-hidden transition-all duration-300 ease-in-out'
         }`}>
               {moduleName[Object.keys(moduleName)[0]].map((topics)=>(
                 <li className='list-disc pl-14 p-5 list-inside odd:bg-gradient-to-r from-gray-800 even:bg-slate-900 rounded-xl'>{topics}</li>
