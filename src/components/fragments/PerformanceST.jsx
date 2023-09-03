22c3b9aec0b255044a5ad3b1ad38e4a16552367a
Srinivasan <jpsrinivasan38@gmail.com>
2023-09-03 14:44:31 +0530

    performance page basic function done

--- src/components/fragments/PerformanceST.jsx
+++ src/components/fragments/PerformanceST.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {darkColors} from "../../config/styleClass"
+import TopNav from '../TopNav'
+import CourseCards from '../CourseCards'
+import { auth, db } from '../../config/firebase'
+import { collection, getDoc, getDocs, doc } from 'firebase/firestore'
+function Courses({wideBar, setFragment}) {
+  const [coursesCollections, setCoursesCollections] = useState([])
+  
+  useEffect(()=>{
+    (async ()=>{
+        const courseRef = collection(db, "user",auth.currentUser.uid,"registeredCourses")
+        const collectionSnap = await getDocs(courseRef)
+        const newData = [];
+        setCoursesCollections([])
+        collectionSnap.forEach(async(courseId)=>{
+          
+          const detailsRef = doc(db, "courses", courseId.id)
+          const detailsSnap = await getDoc(detailsRef)
+          const newColl = {...detailsSnap.data(), id: courseId.id}
+          setCoursesCollections(old=>[...old, newColl])
+      });
+    })()
+}, [])
 
-function PerformanceST({wideBar}) {
+
+
   return (
-    <div  className={wideBar?"ml-[12em]":"ml-16"}>Performance</div>
+    <div className={wideBar?darkColors["mainD"]+" blur-sm md:filter-none md:ml-[12em]":darkColors["mainD"]}>
+       <TopNav fragmentName={"Registered Courses"} 
+       wideBar={wideBar}/>
+       <div className=' w-[80%] grid grid-flow-row mt-16 grid-cols-1 xl:grid-cols-2 xl:gap-10 '>
+        {coursesCollections.length<=0?( <p>No courses available</p> ): 
+        coursesCollections.map((course)=>
+        (
+          <CourseCards setFragment={setFragment} 
+          title ={course["Title"]}
+          author = {course["author"]}
+          sDescription = {course["sDescription"]}
+          rating = {course["ratings"]}
+          id={course["id"]}
+          thumbnail = {course["thumbnail"]}
+          />
+        ))
+        }
+       </div>
+    </div>
   )
 }
 
-export default PerformanceST
+export default Courses
