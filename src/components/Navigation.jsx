17ab1bca18afe57ab0f19fd7f7e4641d0e61fc66
Srinivasan <jpsrinivasan38@gmail.com>
2023-09-04 12:27:15 +0530

    Navigation UI updata

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,7 +5,7 @@ function Navigation({setPage}) {
   const [showMenu, setShowMenu] = useState(false)
   return (
 
-    <div className="flex flex-col md:flex-row justify-between fixed md:w-full max-w-full flex-wrap bg-white md:p-[1em] p-[.5em] top-0">
+    <div className="flex flex-col md:flex-row justify-between fixed w-full max-w-full flex-wrap bg-white md:p-[1em] p-[.5em] top-0 left-0">
       <div className="flex flex-row items-center w-screen md:w-fit">
       <img src={logo}alt="Logo" className="w-5 h-5 md:w-10 md:h-10"/>
         <h1 className="mx-5 font-bold"><span className="text-orange-400">ROXs</span> Academy.</h1>
@@ -19,12 +19,12 @@ function Navigation({setPage}) {
       </div>
       
         <nav className={showMenu?"flex":"hidden"+" md:flex rounded-md p-2 pr-4 shadow-lg"}>
-          <ul className="flex flex-row justify-evenly items-center flex-wrap">
-            <li className="w-full md:w-20 text-end md:text-start "><a href="#home" >Home</a></li>
-            <li className="w-full md:w-20 text-end md:text-start "><a href="#courses">Courses</a></li>
-            <li className="w-full md:w-20 text-end md:text-start "><a href="#about">About</a></li>
-            <li className="w-full md:w-20 text-end md:text-start "><a href="#contact">Contact</a></li>
-            <li className=" border rounded-full bg-cyan-500 text-white font-bold text-sm shadow-xl cursor-pointer ml-auto md:ml[1em] md:mr-[3em] " role="button" tabIndex={0}><a onClick={()=>setPage("login")} className="w-[5.5em] h-[2.5em] flex justify-center items-center">Login</a></li>
+          <ul className="flex w-full flex-row justify-evenly items-center flex-wrap">
+            <li className="mt-2 w-full md:w-20 text-end md:text-start "><a href="#home" className='w-full inline-block text-center' >Home</a></li>
+            <li className="mt-2 w-full md:w-20 text-end md:text-start "><a href="#courses" className='w-full inline-block text-center'>Courses</a></li>
+            <li className="mt-2 w-full md:w-20 text-end md:text-start "><a href="#about" className='w-full inline-block text-center'>About</a></li>
+            <li className=" mt-2 w-full md:w-20 text-end md:text-start "><a href="#contact" className='w-full inline-block text-center'>Contact</a></li>
+            <li className="mt-2 border rounded-full bg-cyan-500 text-white font-bold text-sm shadow-xl cursor-pointer ml-auto md:ml[1em] md:mr-[3em] mx-auto" role="button" tabIndex={0}><a onClick={()=>setPage("login")} className="w-[5.5em] h-[2.5em] flex justify-center items-center">Login</a></li>
           </ul>
         </nav>
     </div>
