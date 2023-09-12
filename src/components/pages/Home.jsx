49aa4ef0b4dc8e207248761006beea69c796c8e7
Srinivasan <jpsrinivasan38@gmail.com>
2023-09-12 13:35:20 +0530

    unknown

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,9 +9,11 @@ const Home = ({setPage}) => {
     <div id="home" className="mt-[66px] bg-white"
     >
       <Navigation setPage={setPage}/>
-      <section className=' w-screen h-screen bg-[#0c1015] mt-[10em]'>
+      <section className=' w-screen h-screen bg-[#0c1015] md:mt-[10em]'>
         <img src={planet} alt="" className='absolute animate-orbit opacity-70 md:-right-[10em] -right-28' />
-        <div className='text-white w-[80%] h-[80%] backdrop-blur-sm backdrop-filter bg-opacity-70 rounded-2xl shadow-2xl mx-auto z-10 shadow-gray-600 block'>
+        <div className='text-white w-[85%] h-[80%] backdrop-blur-sm backdrop-filter bg-opacity-70 rounded-2xl shadow-md mx-auto z-10 shadow-gray-700 flex justify-center items-center'>
+          <h1 className=' font-bold text-4xl' >Welcome to <br /> <span className=' font-SpaceMono text-orange-400'>ROXs</span> Academy</h1>
+          <p></p>
         </div>
 
       </section>
