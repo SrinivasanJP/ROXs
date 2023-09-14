1e422e67737c300386d0e87b7a2f43d4402943ad
Srinivasan <jpsrinivasan38@gmail.com>
2023-09-14 11:45:13 +0530

    Warning fix

--- src/components/pages/BasicDetails.jsx
+++ src/components/pages/BasicDetails.jsx
@@ -129,7 +129,7 @@ const handleSubmit = async(e)=>{
             
             <button className="inline-flex items-center px-4 justify-center py-2 mt-5 font-bold leading-6 text-sm shadow rounded-md text-white bg-cyan-500 min-w-[7em] transition ease-in-out duration-150">
             <svg className={pState?"animate-spin -ml-1 mr-3 h-5 w-5 text-white":"hidden"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
             </svg>{pState?"Processing...":"Submit"}
               </button>
