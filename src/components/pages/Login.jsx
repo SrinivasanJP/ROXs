575b230b8e2909b94ff1862c311fa27c14109927
Srinivasan <jpsrinivasan38@gmail.com>
2023-09-15 11:24:35 +0530

    UI login and signup update

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -62,12 +62,12 @@ function Login({setPage}) {
 
 
   return (
-    <div className="bg-[#f6f6f6] h-screen  flex justify-center items-center">
+    <div className=" bg-[#0c1015] h-screen  flex justify-center items-center">
 
-      <div className="bg-[#fefefe] w-[90%]  rounded-2xl shadow-2xl flex flex-wrap md:p-10 p-2 ">
+      <div className=" backdrop-blur-sm shadow-inner w-[90%] shadow-gray-400  rounded-2xl flex flex-wrap md:p-10 p-2 ">
         <img src={LoginSVG} alt="Login Svg" className="w-1/2 p-5 hidden md:block" />
         <div className="w-full md:w-1/2 p-5 flex flex-col justify-center">
-          <h1 className="antialiased font-extrabold font text-3xl text-left mb-10">Login</h1>
+          <h1 className="font-extrabold font text-3xl text-left mb-10 bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] bg-clip-text text-transparent">Login</h1>
           <form className='mt-5' onSubmit={(e)=> e.preventDefault()}>
             <label htmlFor="uname" className="absolute pt-4 pl-2"><FaUser/></label>
             <input type="text" name="uname" id="uname" placeholder="Enter your User name" title="Username" className="border-b-2 w-full pl-8 p-3 mb-6  bg-stone-100 rounded-2xl shadow-sm" onChange={(e) => setEmail(e.target.value)}/>
@@ -75,9 +75,9 @@ function Login({setPage}) {
             <label htmlFor="uname" className="absolute pt-4 pl-2 block"><BsFillShieldLockFill/></label>
             <input type={passwordVisibility} name="password" id="password" placeholder="Enter your password" title="Password" className="border-b-2 w-full pl-8 p-3 bg-stone-100 rounded-2xl shadow-sm" onChange={(e) => setPassword(e.target.value)}/>
             <input type="checkbox" name="save" id="save" className='mt-5' />
-            <label htmlFor="save" className='mx-2'>Remember me</label>
-            <a onClick={()=>setPage("signup")} className='block mt-5 underline font-mono cursor-pointer'>Create an account</a>
-            <button className="inline-flex items-center px-4 justify-center py-2 mt-5 font-bold leading-6 text-sm shadow rounded-md text-white bg-purple-500 min-w-[7em] transition ease-in-out duration-150" onClick={onLoginClick}>
+            <label htmlFor="save" className='mx-2 text-white'>Remember me</label>
+            <a onClick={()=>setPage("signup")} className='block mt-5 underline font-mono cursor-pointer text-white'>Create an account</a>
+            <button className="bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] inline-flex items-center px-4 justify-center py-2 mt-5 font-bold leading-6 text-sm shadow rounded-md text-gray-900 min-w-[7em] transition ease-in-out duration-150" onClick={onLoginClick}>
             <svg className={login?"animate-spin -ml-1 mr-3 h-5 w-5 text-white":"hidden"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -25,11 +25,11 @@ const createNewUser = async (email, password) =>{
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   return (
-    <div className="bg-[#f6f6f6] h-screen  flex justify-center items-center">
-      <div className="bg-[#fefefe] w-[90%] rounded-2xl shadow-2xl flex flex-wrap md:p-10 p-2">
+    <div className=" bg-[#0c1015] h-screen  flex justify-center items-center">
+      <div className=" shadow-gray-400 shadow-inner w-[90%] rounded-2xl flex flex-wrap md:p-10 p-2">
         <img src={SignUPSVG} alt="Login Svg" className="w-1/2 p-5 hidden md:block" />
         <div className="w-full md:w-1/2 p-5 flex flex-col justify-center">
-          <h1 className="antialiased font-extrabold font text-3xl text-left mb-10">Sign up</h1>
+          <h1 className="bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] bg-clip-text text-transparent font-extrabold font text-3xl text-left mb-10">Sign up</h1>
           <form className='mt-5' onSubmit={(e)=>{return e.preventDefault()}}>
            
             <label htmlFor="uname" className="absolute pt-4 pl-2"><FaUser/></label>
@@ -41,9 +41,9 @@ const createNewUser = async (email, password) =>{
             onChange={(e)=> setPassword(e.target.value)}/>
             <input type="checkbox" name="save" id="save" className='inline-block mt-5'
             />
-            <label htmlFor="save" className='mx-2'>Remember me</label>
-            <a onClick={()=>setPage("login")} className='block mt-5 underline font-mono cursor-pointer'>back to login</a>
-            <button className="inline-flex items-center px-4 justify-center py-2 mt-5 font-bold leading-6 text-sm shadow rounded-md text-white bg-cyan-500 min-w-[7em] transition ease-in-out duration-150" onClick={()=>createNewUser(email, password)}>
+            <label htmlFor="save" className='mx-2 text-white'>Remember me</label>
+            <a onClick={()=>setPage("login")} className='block mt-5 underline font-mono cursor-pointer text-white'>back to login</a>
+            <button className="inline-flex items-center px-4 justify-center py-2 mt-5 font-bold leading-6 text-sm shadow rounded-md bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,_#7ED4FD_14.06%,#709DF7_51.02%,#4D78EF_79.09%)] text-gray-900 min-w-[7em] transition ease-in-out duration-150" onClick={()=>createNewUser(email, password)}>
             <svg className={signUp?"animate-spin -ml-1 mr-3 h-5 w-5 text-white":"hidden"} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
               <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
               <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
